7429a8dea87f5021ffb8a3514dd4797d23446173
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2021-12-23 17:36:01 -0300

    chore: front-end deployment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,10 +2,13 @@ export const environment = {
   production: true,
   services: {
     backend: {
-      BASE_URL: 'http://localhost:3000',
-      GET_PARTIES_PATH: '',
-      GET_PARTIES_WITH_MEMBERS_PATH: '',
-      GET_CONGRESSPEOPLE_PATH: '',
+      BASE_URL: 'https://politicaltracker-mac413.herokuapp.com',
+      GET_PARTIES_PATH: '/party',
+      GET_PARTIES_WITH_MEMBERS_PATH: '/party?members=1',
+      GET_CONGRESSPEOPLE_PATH: '/congressperson',
+      GET_MONITOR_VOTINGS: '/monitor',
+      GET_MONITOR_SUBJECTS: '/subjects-monitor/',
+      GET_MONITOR_LAW_COUNTS: '/law-counts',
     },
   },
 };
